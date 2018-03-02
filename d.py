c96e716f68df360e13c549a732fdd62041b51036
Isterikus <kovalenko27071996@gmail.com>
2018-03-02 19:30:38 +0200

    started d

---
+++ d.py
@@ -0,0 +1,16 @@
+
+n, s, f = list(map(int, input().split(' ')))
+gates_col = list(map(int, input().split(' ')))
+gates_keys = list(map(int, input().split(' ')))
+
+keys_exist = set()
+s -= 1
+f -= 1
+now = s
+
+keys_need = set()
+for i in range(min(s, f), max(s, f)):
+	keys_need.add()
+
+while True:
+	keys_exist.add(gates_keys[now])
