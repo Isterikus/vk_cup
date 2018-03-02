dd4e2fb32e05491ed199e449f003225afc4a5bb6
Isterikus <kovalenko27071996@gmail.com>
2018-03-02 21:22:08 +0200

    d in poor progress...

--- d.py
+++ d.py
@@ -1,4 +1,3 @@
-
 n, s, f = list(map(int, input().split(' ')))
 gates_col = list(map(int, input().split(' ')))
 gates_keys = list(map(int, input().split(' ')))
@@ -8,9 +7,7 @@ s -= 1
 f -= 1
 now = s
 
-keys_need = set()
-for i in range(min(s, f), max(s, f)):
-	keys_need.add()
+keys_need = set(gates_keys[min(s, f): max(s, f) + 1])
 
 while True:
-	keys_exist.add(gates_keys[now])
+    keys_exist.add(gates_keys[now])
